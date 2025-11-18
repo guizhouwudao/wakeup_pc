36c6e8d7b7fe5a879c44b7ec95f5767174b58579
guizhouwudao <62141922+guizhouwudao@users.noreply.github.com>
2025-11-18 19:37:23 +0800

    Update wakeup.c

--- c/wakeup.c
+++ c/wakeup.c
@@ -31,8 +31,6 @@ Config config;
 
 // 执行命令
 char cmd_shutdown[1024];
-char cmdos[1024];
-char appds[1024];
 // 套接字持续化连接
 int tcp_client_socket = -1;
 
@@ -224,7 +222,7 @@ int wol(const char *mac)
 void init_cmd()
 {
     // #局域网连接openssh服务器，进行关机操作
-    snprintf(cmd_shutdown, sizeof(cmd_shutdown),"sshpass -p %s ssh -A -g -o StrictHostKeyChecking=no %s@%s", config.commands, config.password, config.user, config.ip);
+    snprintf(cmd_shutdown, sizeof(cmd_shutdown),"sshpass -p %s ssh -A -g -o StrictHostKeyChecking=no %s@%s %s", config.password, config.user, config.ip, config.commands);
     // 打印命令
     printf("cmd_shutdown: %s\n", cmd_shutdown);
 }
